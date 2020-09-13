18d35045c8aa23d0717c1536b35f2aa810a7bca4
Timohenciy <dox1gun@gmail.com>
2020-09-13 20:32:34 +0300

    Ошибка

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
     let mobileMenu = document.querySelector('.mobile-menu');
     let mobileMenuOpenBtn = mobileMenu.querySelector('.fa-bars');
     let firstLevelMenu = mobileMenu.querySelector('.mobile_menu-level-1');
-    let submenuMobile = mobileMenu.querySelector('.submenu_window-mobile');
 
     mobileMenuOpenBtn.addEventListener('click', function () {
         mobileMenuOpenBtn.classList.toggle('fa-bars');
@@ -17,10 +16,6 @@
             e.target.previousElementSibling.classList.toggle('open_dropdown_menu');
             
         }
-        console.log(e.composedPath());
-    });
-
-    submenuMobile.addEventListener('click', function (e) {
-        console.log(e.target);
+        
     });
 }());
