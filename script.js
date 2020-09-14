b7f1de534af82cd2be6c81a6affeb1f2ef28b2d6
Timohenciy <dox1gun@gmail.com>
2020-09-14 18:31:54 +0300

    При открытии пункта меню ранее открытые пункты меню заккрываются

--- script.js
+++ script.js
@@ -11,11 +11,32 @@
         firstLevelMenu.classList.toggle('open_dropdown_menu');
     });
 
+    let checkCurrentLevelClasses = function (clickTargetParent) {
+        let elementsCollection = clickTargetParent.parentElement.children;
+
+        for (let element of elementsCollection) {
+            let submenuElementsCollection = element.children[1].children;
+
+            for (let subElement of submenuElementsCollection) {
+                if (subElement.children[1] && subElement.children[1].classList.contains('open_dropdown_menu')) {
+                    subElement.children[1].classList.remove('open_dropdown_menu');
+                }
+            }
+
+            if (element != clickTargetParent && element.children[1].classList.contains('open_dropdown_menu')) {
+                element.children[1].classList.remove('open_dropdown_menu');
+            }
+        };
+    };
+
     firstLevelMenu.addEventListener('click', function (e) {
         if (e.target.classList.contains('fa-angle-down')) {
-            e.target.previousElementSibling.classList.toggle('open_dropdown_menu');
             
+            e.target.previousElementSibling.classList.toggle('open_dropdown_menu');
+            checkCurrentLevelClasses(e.target.parentElement);
+            
+            e.target.parentElement.scrollIntoView({block: 'start', behavior: 'smooth'});
         }
-        
     });
+
 }());
